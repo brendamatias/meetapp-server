54fbaf73a2713bbe3853c92de9f10df9757710fb
Brenda Matias <brendamatias.sobral@gmail.com>
2019-09-30 21:47:21 -0300

    feat: added user and token

---
+++ src/app/models/User.js
@@ -0,0 +1,39 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      /*
+        Validação para o hash ser gerado apenas quando tiver
+        inserindo ou alterando a senha
+      */
+      if (user.password) {
+        /*
+          8: número de rounds(força) da minha criptografia
+        */
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
