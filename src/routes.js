f31744cf0ab53946f2d1595ffdf1962e913a1f76
Brenda Matias <brendamatias.sobral@gmail.com>
2019-09-30 23:48:48 -0300

    feat: added meetups

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
+import OrganizingController from './app/controllers/OrganizingController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,8 +16,16 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.post('/files', upload.single('file'), FileController.store);
 routes.use(authMiddleware);
+routes.post('/files', upload.single('file'), FileController.store);
+
 routes.put('/users', UserController.update);
+
+routes.get('/meetups', MeetupController.index);
+routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.get('/organizing', OrganizingController.index);
 
 export default routes;
