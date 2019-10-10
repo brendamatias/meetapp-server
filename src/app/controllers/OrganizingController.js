c3dae46e85ed3f78c76b52f0b48d6bc4210e426b
Brenda Matias <brendamatias.sobral@gmail.com>
2019-10-10 19:39:17 -0300

    Correção url imagem

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,4 +1,5 @@
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class OrganizingController {
   async index(req, res) {
@@ -12,6 +13,32 @@ class OrganizingController {
     });
 
     return res.json(meetups);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    const meetup = await Meetup.findByPk(id, {
+      include: [
+        {
+          model: File,
+          as: 'file',
+          attributes: ['id', 'name', 'path', 'url'],
+        },
+      ],
+    });
+
+    if (!meetup) {
+      return res.status(400).json({ error: 'Meetup does not exists' });
+    }
+
+    if (meetup.user_id !== req.userId) {
+      return res.status(400).json({
+        error: "You don't have permission to see meetup from other users",
+      });
+    }
+
+    return res.json(meetup);
   }
 }
 
